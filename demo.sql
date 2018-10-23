720b0ba8756f7a76354abb8570b5788761a43729
wenlinzou <daoxiji@gmail.com>
2018-10-23 09:12:34 +0800

    数据库设计

---
+++ demo.sql
@@ -0,0 +1,84 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50720
+Source Host           : 127.0.0.1:3306
+Source Database       : demo
+
+Target Server Type    : MYSQL
+Target Server Version : 50720
+File Encoding         : 65001
+
+Date: 2018-10-22 15:09:50
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for order_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE `order_detail` (
+  `detail_id` varchar(32) NOT NULL,
+  `order_id` varchar(32) NOT NULL,
+  `product_id` varchar(32) NOT NULL,
+  `product_name` varchar(64) NOT NULL COMMENT '商品名称',
+  `product_price` decimal(8,2) NOT NULL COMMENT '商品价格',
+  `product_quantity` int(255) NOT NULL COMMENT '商品数量',
+  `product_icon` varchar(512) NOT NULL COMMENT '商品小图',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`detail_id`),
+  KEY `idx_order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for order_master
+-- ----------------------------
+DROP TABLE IF EXISTS `order_master`;
+CREATE TABLE `order_master` (
+  `order_id` varchar(32) NOT NULL,
+  `buyer_name` varchar(32) NOT NULL COMMENT '买家名字',
+  `buyer_phone` varchar(32) NOT NULL COMMENT '买家手机',
+  `buyer_address` varchar(128) NOT NULL COMMENT '买家地址',
+  `buyer_openid` varchar(64) NOT NULL COMMENT '买家微信openid',
+  `order_amount` decimal(8,2) NOT NULL COMMENT '订单总金额',
+  `order_status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '订单状态，默认0新下单',
+  `pay_status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '支付状态，默认0未支付',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`),
+  KEY `idx_buyer_openid` (`buyer_openid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for product_category
+-- ----------------------------
+DROP TABLE IF EXISTS `product_category`;
+CREATE TABLE `product_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(64) NOT NULL COMMENT '类目名称',
+  `category_type` int(11) NOT NULL COMMENT '类目编号',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `uqe_category_type` (`category_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for product_info
+-- ----------------------------
+DROP TABLE IF EXISTS `product_info`;
+CREATE TABLE `product_info` (
+  `product_id` varchar(32) NOT NULL COMMENT '商品编号',
+  `product_name` varchar(64) NOT NULL COMMENT '商品名称',
+  `product_price` decimal(8,2) NOT NULL COMMENT '商品单价',
+  `product_stock` int(11) NOT NULL COMMENT '库存',
+  `product_descrption` varchar(64) DEFAULT NULL COMMENT '描述',
+  `product_icon` varchar(512) DEFAULT NULL COMMENT '小图',
+  `category_type` int(255) NOT NULL COMMENT '类目编号',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
